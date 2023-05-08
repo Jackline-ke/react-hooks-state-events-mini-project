6373ba4466450b96c0b81ff4898b32fddad8c1cb
Jackline-ke <antoninahodhiambo@gmail.com>
2023-05-08 08:42:19 +0300

    Completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, selectedCategory, onCategorySelect }) {
+
+  const handleCategoryClick = (category) => {
+    onCategorySelect(category);
+  };
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={category === selectedCategory ? 'selected' : ''}
+          onClick={() => handleCategoryClick(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
-
 export default CategoryFilter;
